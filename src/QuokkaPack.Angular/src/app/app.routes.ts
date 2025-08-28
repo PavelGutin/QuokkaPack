670309ccc482137037e2159185547fa4c9362d47
Pavel Gutin <pavelgutin@gmail.com>
2025-08-28 16:26:58 -0400

    Prune old code

--- src/QuokkaPack.Angular/src/app/app.routes.ts
+++ src/QuokkaPack.Angular/src/app/app.routes.ts
@@ -5,7 +5,6 @@ export const routes: Routes = [
   { path: 'login', loadComponent: () => import('./login/login').then(m => m.Login) },
   { path: 'trips', loadComponent: () => import('./trips/trips-page/trips-page').then(m => m.TripsPage) },
   { path: 'trips/:id/pack', loadComponent: () => import('./trips/trip-pack/trip-pack').then(m => m.TripPack) },
-  { path: 'trips/:id/edit', loadComponent: () => import('./trips/trip-edit/trip-edit').then(m => m.TripEdit) },
   { path: 'items', loadComponent: () => import('./items/items').then(m => m.Items) },
   { path: '**', redirectTo: '' } // 404 fallback
 ];
